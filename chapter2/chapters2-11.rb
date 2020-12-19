ff7f2970f6f87f41cb9f40be54626dfc12748ca0
MASA20201203 <koedame032@gmail.com>
2020-12-20 07:01:26 +0900

    2.11.1 デフォルト値付きの引数

--- chapter2/chapters2-11.rb
+++ chapter2/chapters2-11.rb
@@ -1,5 +1,50 @@
 # 2.11 メソッド定義についてもっと詳しく
 
-def greeting(country)
-  if
+# 2.11.1 デフォルト値付きの引数
+# def greeting(country)
+#   if country == 'japan'
+#     'こんにちは'
+#   else
+#     'hello'
+#   end
+# end
+
+# 引数が少ない
+# puts greeting
+
+# 引数がちょうど
+# puts greeting('us')
+
+# 引数が多い
+# puts greeting('us', 'japan')
+
+# 引数なしの場合はcountryに'japan'を設定する
+# def greeting(country = 'japan')
+#   if country == 'japan'
+#     'こんにちは'
+#   else
+#     'hello'
+#   end
+# end
+
+# puts greeting
+# puts greeting('us')
+
+# 引数のデフォルト値は混在可能
+# def default_args(a, b, c = 0, d = 0)
+#   "a=#{a}, b=#{b}, c=#{c}, d=#{d}"
+# end
+# puts default_args(1, 2)
+# puts default_args(1, 2, 3)
+# puts default_args(1, 2, 3, 4)
+
+# システム日時や他のメソッドの戻り値をデフォルト値に指定する
+def foo(time = Time.now, message = bar)
+  puts "time: #{time}, message: #{message}"
 end
+
+def bar
+  'BAR'
+end
+
+foo
