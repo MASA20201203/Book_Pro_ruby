2452eeb96fdd71b5b46ca462c04f23ad792a259f
MASA20201203 <koedame032@gmail.com>
2020-12-19 10:41:01 +0900

    2.3.1 シングルクオートとダブルクオート

---
+++ chapter2/chapters2-3.rb
@@ -0,0 +1,31 @@
+# 2.3.1 シングルクオートとダブルクオート
+# puts 'これは文字列です。'
+# puts "これも文字列です。"
+
+# ダブルクオートで囲むと\nが改行文字として機能する
+# puts "こんにちは\nさようなら"
+
+# シングルクオートで囲むと\nはただの文字列になる
+# puts 'こんにちは\nさようなら'
+
+# name = 'Alice'
+# puts "Hello, #{name}!"
+
+# i = 10
+# puts "#{i}は16進数にすると#{i.to_s(16)}です"
+
+# name = 'Alice'
+# puts 'Hello, #{name}!'
+
+# name = 'Alice'
+# puts 'Hello, ' + name + '!'
+
+
+# puts "こんにちは\\nさようなら"
+
+# name = 'Alice'
+# puts "Hello, \#{name}!"
+
+puts 'He said, "Don\'t speak."'
+puts "He said, \"Don't speak.\""
+
