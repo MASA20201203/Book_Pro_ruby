b7b1b0b558aad6ff7c5e1b6c0cedf729af2d6c90
MASA20201203 <koedame032@gmail.com>
2020-12-19 09:47:48 +0900

    2.2.3 文の区切り

--- chapter2/chapters2.rb
+++ chapter2/chapters2.rb
@@ -12,3 +12,32 @@
 # puts 10.to_s(16)
 # puts 10.to_s 16
 
+# 2.2.2 メソッド呼び出し
+# puts 1.to_s()
+# puts 1.to_s
+# puts 10.to_s(16)
+# puts 10.to_s 16
+
+# 2.2.3 文の区切り
+
+# puts '改行ごとにメソッドが実行される'
+# puts 1.to_s
+# puts nil.to_s
+# puts 10.to_s(16)
+
+# puts 'セミコロンを使って、3つの文を1行に押し込める'
+# puts 1.to_s; puts nil.to_s; puts 10.to_s(16)
+
+# puts '( で改行しているので、カッコが閉じられるまで改行してもエラーにならない'
+# puts 10.to_s(
+#   16
+# )
+
+
+# puts '( がない場合は10.to_sと16という2つの文だとみなされる'
+# puts 10.to_s
+# puts 16
+
+# puts 'バックシュラッシュを使って10.to_s 16を改行して書く'
+# puts 10.to_s \
+# 16
