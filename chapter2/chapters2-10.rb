093a4a35f1b1843ac15911419bc188f276de376f
MASA20201203 <koedame032@gmail.com>
2020-12-19 15:54:31 +0900

    2.10.2 優先順位が低いand、or、not

--- chapter2/chapters2-10.rb
+++ chapter2/chapters2-10.rb
@@ -15,4 +15,38 @@
 # 正常なユーザであればメールを送信する（左辺が偽であればメール送信は実行されない）
 # user.valid? && send_email_to(user)
 
+# 2.10.2 優先順位が低いand、or、not
+# t1 = true
+# f1 = false
+# puts (t1 and f1)
+# puts t1 or f1
+# puts (not t1)
 
+# t1 = true
+# f1 = false
+# puts (!f1 || t1)
+# puts (not f1 || t1)
+
+# # !は||よりも優先順位が高い
+# !(f1) || t1
+# # notは||よりも優先順位が低い
+# not(f1 || t1)
+
+# t1 = true
+# t2 = true
+# f1 = false
+# puts (t1 || t2 && f1) # &&は||よりも優先順位が高い
+# puts (t1 or t2 and f1) # andとorの優先順位は同じなので、左から順に評価される
+
+def greeting(country)
+  # countryがnil（またはfalse）ならメッセージを返してメソッドを抜ける
+  country or return 'countryを入力してください'
+
+  if country == 'japan'
+    'こんにちは'
+  else
+    'hello'
+  end
+end
+puts greeting(nil)
+puts greeting('japan')
