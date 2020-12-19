1f6f0099db82baa47613fda6f171cbff481697b2
MASA20201203 <koedame032@gmail.com>
2020-12-19 14:23:47 +0900

    FizzBuzzプログラム完成

--- chapter2/chapters2-7.rb
+++ chapter2/chapters2-7.rb
@@ -1,2 +1,21 @@
 # 2.7 例題：FizzBuzzプログラムを作成する
-# 2.7.1 作業用のディレクトリとファイルを準備する
+# 2.7.3 fizz_buzzメソッドを作成する
+def fizz_buzz(n)
+  if n % 15 == 0
+    'Fizz Buzz'
+  elsif n % 3 == 0
+    'Fizz'
+  elsif n % 5 == 0
+    'Buzz'
+  else
+    n.to_s
+  end
+end
+
+puts fizz_buzz(1)
+puts fizz_buzz(2)
+puts fizz_buzz(3)
+puts fizz_buzz(4)
+puts fizz_buzz(5)
+puts fizz_buzz(6)
+puts fizz_buzz(15)
