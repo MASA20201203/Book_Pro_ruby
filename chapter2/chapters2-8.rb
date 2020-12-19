85c8bd4306ba352b4f139ed3b843c7407e7c7a1e
MASA20201203 <koedame032@gmail.com>
2020-12-19 14:53:10 +0900

    2.8.3 ヒアドキュメント（行指向文字列リテラル）

--- chapter2/chapters2-8.rb
+++ chapter2/chapters2-8.rb
@@ -1,7 +1,7 @@
 # 2.8 文字列についてもっと詳しく
 
 # 2.8.1 文字列はStringクラスのオブジェクト
-puts 'abc'.class
+# puts 'abc'.class
 
 # 2.8.2 %記法で文字列を作る
 # # %q! !はシングルクオートで囲んだことと同じになる
@@ -21,3 +21,81 @@ puts 'abc'.class
 # # { }を区切り文字としてつかう
 # puts %q{He said, "Don't speak."}
 
+# 2.8.3 ヒアドキュメント（行指向文字列リテラル）
+# 文字列は途中で改行できる
+# puts "Line 1,
+# Line 2"
+
+# puts 'Line 1,
+# Line 2'
+
+# a = <<TEXT
+# これはヒアドキュメントです。
+# 複数行に渡る長い文字列を作成するのに便利です。
+# TEXT
+# puts a
+
+# b = <<HTML
+# <div>
+#   <img src="sample.jpg">
+# </div>
+# HTML
+# puts b
+
+# 最期の識別子をインデント
+# def some_method
+#   <<-TEXT
+#     これはヒアドキュメントです。
+#     <<-を使うと最期の識別子をインデントさせることができます。
+#   TEXT
+# end
+
+# puts some_method
+
+# <<~TEXT
+# def some_method2
+#   <<~TEXT
+#     これはヒアドキュメントです。
+#     <<~を使うと内部文字列のインデント部分が無視されます。
+#   TEXT
+# end
+
+# puts some_method2
+
+# ヒアドキュメントの中では式展開が有効
+# name = 'Alice'
+# a = <<TEXT
+# ようこそ、#{name}さん！
+# 以下のメッセージをご覧ください。
+# TEXT
+# puts a
+
+# name = 'Alice'
+# a = <<'TEXT'
+# ようこそ、#{name}さん！
+# 以下のメッセージをご覧ください。
+# TEXT
+# puts a
+
+# name = 'Alice'
+# a = <<"TEXT"
+# ようこそ、#{name}さん！
+# 以下のメッセージをご覧ください。
+# TEXT
+# puts a
+
+# # ヒアドキュメントを直接引数として渡す（prependは渡された文字列を先頭に追加するメソッド）
+# a = 'Ruby'
+# a.prepend(<<TEXT)
+# Java
+# PHP
+# TEXT
+# puts a
+
+# # ヒアドキュメントで作成した文字列に対して、直接upcaseメソッドを呼び出す
+# # (upcaseは文字列をすべて大文字にするメソッド)
+# b = <<TEXT.upcase
+# Hello,
+# Good-bye.
+# TEXT
+# puts b
