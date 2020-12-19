2f91c4875d8a1d1560b1a688c702db46e32b728c
MASA20201203 <koedame032@gmail.com>
2020-12-19 09:33:33 +0900

    2.2.2 メソッド呼び出し

---
+++ chapter2/chapters2.rb
@@ -0,0 +1,14 @@
+# 2.2.1 すべてがオブジェクト
+# puts '1'.to_s
+# puts 1.to_s
+# puts nil.to_s
+# puts true.to_s
+# puts false.to_s
+# puts /\d+/.to_s
+
+# 2.2.2 メソッド呼び出し
+# puts 1.to_s()
+# puts 1.to_s
+# puts 10.to_s(16)
+# puts 10.to_s 16
+
