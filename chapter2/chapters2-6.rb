982dd424f6951f3a12ffeac9be7babba574b739a
MASA20201203 <koedame032@gmail.com>
2020-12-19 14:08:14 +0900

    2.6.1 メソッドの戻り値

--- chapter2/chapters2-6.rb
+++ chapter2/chapters2-6.rb
@@ -1,8 +1,34 @@
 # 2.6 メソッドの定義
 
 # 2.6.1 メソッドの戻り値
-def add(a, b)
-  # return もつかるが、使わない方が主流
-  return a + b
+# def add(a, b)
+#   # return もつかるが、使わない方が主流
+#   return a + b
+# end
+# puts add(1, 2)
+
+# def greeting(country)
+#   # "こんにちは"または"hello"がメソッドの戻り値になる
+#   if country == 'japan'
+#     'こんにちは'
+#   else
+#     'hello'
+#   end
+# end
+# puts greeting('japan')
+# puts greeting('us')
+
+def greeting(country)
+  # countryがnilならメッセージを返してメソッドを抜ける
+  # (nil?はオブジェクトがnilの場合にtrueを返すメソッド）
+  return 'countryを入力してください' if country.nil?
+
+  if country == 'japan'
+    'こんにちは'
+  else
+    'hello'
+  end
 end
-puts add(1, 2)
+puts greeting(nil)
+puts greeting('japan')
+  
