4001b59e07d4a54e2494fa0336943aa3dd2695d6
MASA20201203 <koedame032@gmail.com>
2020-12-19 14:12:54 +0900

    2.6.2 メソッド定義における引数の()

--- chapter2/chapters2-6.rb
+++ chapter2/chapters2-6.rb
@@ -18,17 +18,39 @@
 # puts greeting('japan')
 # puts greeting('us')
 
-def greeting(country)
-  # countryがnilならメッセージを返してメソッドを抜ける
-  # (nil?はオブジェクトがnilの場合にtrueを返すメソッド）
-  return 'countryを入力してください' if country.nil?
+# def greeting(country)
+#   # countryがnilならメッセージを返してメソッドを抜ける
+#   # (nil?はオブジェクトがnilの場合にtrueを返すメソッド）
+#   return 'countryを入力してください' if country.nil?
 
-  if country == 'japan'
-    'こんにちは'
-  else
-    'hello'
-  end
-end
-puts greeting(nil)
-puts greeting('japan')
-  
+#   if country == 'japan'
+#     'こんにちは'
+#   else
+#     'hello'
+#   end
+# end
+# puts greeting(nil)
+# puts greeting('japan')
+
+# 2.6.2 メソッド定義における引数の()
+# 引数がない場合は( )を付けない方が主流
+# def greeting
+#   'こんにちは'
+# end
+# puts greeting
+
+# ()をつけても良いが、省略されることが多い
+# def greeting2()
+#   'hello'
+# end
+# puts greeting2
+
+# ()を省略できるが、引数がある場合は()をつけることの方が多い
+# def greeting3 country
+#   if country == 'japan'
+#     'こんにちは'
+#   else
+#     'ciao'
+#   end
+# end
+# puts greeting3('fc')
