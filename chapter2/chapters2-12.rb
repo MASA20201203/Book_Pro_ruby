4f1dce4d86ed3553935a1137c1835c3f2df900da
MASA20201203 <koedame032@gmail.com>
2020-12-20 08:02:57 +0900

    2.12.5 参照の概念を理解する

--- chapter2/chapters2-12.rb
+++ chapter2/chapters2-12.rb
@@ -1,1 +1,58 @@
 # 2.12 その他の基礎知識
+
+# 2.12.3 式（Expression）と文（Statement）
+# if文が値を返すので変数に代入できる
+# a =
+#   if true
+#     '真です'
+#   else
+#     '偽です'
+#   end
+# puts a
+
+# c = def foo; end
+# p c
+
+# 2.12.4 擬似変数
+# true = 1
+
+# 2.12.5 参照の概念を理解する
+# # aとbはどちらも同じ文字列だが、オブジェクトとしては別物
+# a = 'hello'
+# b = 'hello'
+# puts a.object_id
+# puts b.object_id
+
+# # cにbを代入する。bとcどちらも同じオブジェクト
+# c = b
+# puts c.object_id
+
+# # メソッドの引数にcを渡す。引数として受け取ったdはb、cと同じオブジェクト
+# def m(d)
+#   d.object_id
+# end
+# puts m(c)
+
+# # equal?メソッドを使って同じオブジェクトかどうか確認してもよい
+# puts a.equal?(b)
+# puts b.equal?(c)
+
+# b, cは同じオブジェクト、aは異なるオブジェクト
+a = 'hello'
+b = 'hello'
+c = b
+
+# 渡された文字列を破壊的に大文字に変換するメソッドを定義する
+def m!(d)
+  d.upcase!
+end
+
+# cにm!メソッドを適用する
+m!(c)
+
+# b, cはいずれも大文字になる
+puts b
+puts c
+
+# aは別のオブジェクトなので大文字にならない
+puts a
