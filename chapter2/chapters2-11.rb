aa2ef1e6d9114056b2bda5f615568ed8bdc1d348
MASA20201203 <koedame032@gmail.com>
2020-12-20 07:12:58 +0900

    2.11.2 ?で終わるメソッド

--- chapter2/chapters2-11.rb
+++ chapter2/chapters2-11.rb
@@ -39,12 +39,42 @@
 # puts default_args(1, 2, 3, 4)
 
 # システム日時や他のメソッドの戻り値をデフォルト値に指定する
-def foo(time = Time.now, message = bar)
-  puts "time: #{time}, message: #{message}"
-end
+# def foo(time = Time.now, message = bar)
+#   puts "time: #{time}, message: #{message}"
+# end
 
-def bar
-  'BAR'
-end
+# def bar
+#   'BAR'
+# end
 
-foo
+# foo
+
+# 2.11.2 ?で終わるメソッド
+# # 空文字列であればtrue、そうでなければfalse
+# puts ''.empty?
+# puts 'abc'.empty?
+
+# # 引数の文字列が含まれていなければtrue、そうでなければfalse
+# puts 'watch'.include?('at')
+# puts 'watch'.include?('in')
+
+# # 奇数ならtrue、偶数ならfalse
+# puts 1.odd?
+# puts 2.odd?
+
+# # 偶数ならturue、奇数ならfalse
+# puts 1.even?
+# puts 2.even?
+
+# オブジェクトがnilであればtrue、そうでなければfalse
+# puts nil.nil?
+# puts 'abc'.nil?
+# puts 1.nil?
+
+# 3の倍数ならtrue、それ以外はfalseを返す
+# def multiple_of_three?(n)
+#   n % 3 == 0
+# end
+# puts multiple_of_three?(4)
+# puts multiple_of_three?(5)
+# puts multiple_of_three?(6)
