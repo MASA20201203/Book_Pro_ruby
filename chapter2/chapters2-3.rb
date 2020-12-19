182619f1a66bdabac9bba96fa7a01ffab03c99e7
MASA20201203 <koedame032@gmail.com>
2020-12-19 10:45:46 +0900

    2.3.2 文字列の比較

--- chapter2/chapters2-3.rb
+++ chapter2/chapters2-3.rb
@@ -26,6 +26,19 @@
 # name = 'Alice'
 # puts "Hello, \#{name}!"
 
-puts 'He said, "Don\'t speak."'
-puts "He said, \"Don't speak.\""
+# puts 'He said, "Don\'t speak."'
+# puts "He said, \"Don't speak.\""
 
+# 2.3.2 文字列の比較
+# puts 'ruby' == 'ruby'
+# puts 'ruby' == 'Ruby'
+# puts 'ruby' != 'perl'
+# puts 'ruby' != 'ruby'
+
+# puts 'a' < 'b'
+# puts 'a' < 'A'
+# puts 'a' > 'A'
+# puts 'abc' < 'def'
+# puts 'abc' < 'ab'
+# puts 'abc' < 'abcd'
+# puts 'あいうえお' < 'かきくけこ'
