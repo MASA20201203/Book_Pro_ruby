a991714169187f80e06b4e269327fc64ce063c46
MASA20201203 <koedame032@gmail.com>
2020-12-20 11:06:26 +0900

    4.4.3 find/detect

--- chapter4/chapter4-4.rb
+++ chapter4/chapter4-4.rb
@@ -11,12 +11,22 @@
 # p new_numbers
 
 # 4.4.2 select/find_all\reject
-numbers = [1, 2, 3, 4, 5, 6]
-# ブロックの戻り値が真になった要素だけが集められる
-even_numbers = numbers.select { |n| n.even?}
-p even_numbers
+# numbers = [1, 2, 3, 4, 5, 6]
+# # ブロックの戻り値が真になった要素だけが集められる
+# even_numbers = numbers.select { |n| n.even?}
+# p even_numbers
 
-numbers = [1, 2, 3, 4, 5, 6]
-# 3の倍数を除外する（3の倍数以外を集める）
-non_multiples_of_three = numbers.reject { |n| n % 3 == 0 }
-p non_multiples_of_three
+# numbers = [1, 2, 3, 4, 5, 6]
+# # 3の倍数を除外する（3の倍数以外を集める）
+# non_multiples_of_three = numbers.reject { |n| n % 3 == 0 }
+# p non_multiples_of_three
+
+# 4.4.3 find/detect
+# numbers = [1, 2, 3, 4, 5, 6]
+# # ブロックの戻り値が最初に真になった要素を返す
+# even_number = numbers.find { |n| n.even? }
+# p even_number
+
+
+
+
