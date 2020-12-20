6040483c23de1a5f050fab358719e6a0f3ed5768
MASA20201203 <koedame032@gmail.com>
2020-12-20 11:22:25 +0900

    4.5 範囲（Range）

---
+++ chapter4/chapter4-5.rb
@@ -0,0 +1,22 @@
+# 4.5 範囲（Range）
+
+# p (1..5).class
+# p (1...5).class
+
+# p range = 1..5
+# p range.include?(0)
+# p range.include?(1)
+# p range.include?(4.9)
+# p range.include?(5)
+# p range.include?(6)
+
+# p range = 1...5
+# p range.include?(0)
+# p range.include?(1)
+# p range.include?(4.9)
+# p range.include?(5)
+# p range.include?(6)
+
+
+
+
