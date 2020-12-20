b3cf52b8af5f6a388c013bfd226d60933e9f5510
MASA20201203 <koedame032@gmail.com>
2020-12-20 15:11:16 +0900

    2桁への変換方法確認

--- chapter4/rgb.rb
+++ chapter4/rgb.rb
@@ -1,3 +1,14 @@
 def to_hex(r, g, b)
   '#000000'
 end
+
+# 2桁への変換方法確認
+# p 0.to_s(16)
+# p 255.to_s(16)
+
+# p '0'.rjust(5)
+# p '0'.rjust(5, '0')
+# p '0'.rjust(5, '_')
+
+# p 0.to_s(16).rjust(2, '0')
+# p 255.to_s(16).rjust(2, '0')
