bd36f37d89825aab08ede0cc778c98d4dc8c8faf
MASA20201203 <koedame032@gmail.com>
2020-12-20 15:14:44 +0900

    to_hexメソッド実装

--- chapter4/rgb.rb
+++ chapter4/rgb.rb
@@ -1,14 +1,6 @@
 def to_hex(r, g, b)
-  '#000000'
+  '#' +
+  r.to_s(16).rjust(2, '0') +
+  g.to_s(16).rjust(2, '0') +
+  b.to_s(16).rjust(2, '0')
 end
-
-# 2桁への変換方法確認
-# p 0.to_s(16)
-# p 255.to_s(16)
-
-# p '0'.rjust(5)
-# p '0'.rjust(5, '0')
-# p '0'.rjust(5, '_')
-
-# p 0.to_s(16).rjust(2, '0')
-# p 255.to_s(16).rjust(2, '0')
