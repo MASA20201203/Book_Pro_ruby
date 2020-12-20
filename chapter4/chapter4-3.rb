b2e507b51f978d72ad5999b28f788c1d2f014ae7
MASA20201203 <koedame032@gmail.com>
2020-12-20 10:36:39 +0900

    4.3.3 配列の要素を削除する条件を自由に指定する

--- chapter4/chapter4-3.rb
+++ chapter4/chapter4-3.rb
@@ -1,9 +1,24 @@
 # 4.3 ブロック
 
 # 4.3.2 Rubyの繰り返し処理
-numbers = [1, 2, 3, 4]
-sum = 0
-numbers.each do |n|
-  sum += n
-end
-p sum
+# numbers = [1, 2, 3, 4]
+# sum = 0
+# numbers.each do |n|
+#   sum += n
+# end
+# p sum
+
+# 4.3.3 配列の要素を削除する条件を自由に指定する
+# a = [1, 2, 3, 1, 2, 3]
+# p a
+# # 配列から値が2の要素を削除する
+# p a.delete(2)
+# p a
+
+# a = [1, 2, 3, 1, 2, 3]
+# p a
+# # 配列から値が奇数の要素を削除する
+# a.delete_if do |n|
+#   n.odd?
+# end
+# p a
