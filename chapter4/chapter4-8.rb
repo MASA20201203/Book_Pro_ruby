a053c8a71922d61c3c28eeea84500c2a4f53c210
MASA20201203 <koedame032@gmail.com>
2020-12-21 08:07:52 +0900

    4.8.1 添え字付きの繰り返し操作

--- chapter4/chapter4-8.rb
+++ chapter4/chapter4-8.rb
@@ -1,1 +1,6 @@
 #  4.8 ブロックについてもっと詳しく
+
+# 4.8.1 添え字付きの繰り返し操作
+# fruits = ['apple', 'orange', 'melon']
+# # ブロック引数のiには0, 1, 2...と要素の添え字が入る
+# fruits.each_with_index { |fruit, i| puts "#{i}: #{fruit}" }
