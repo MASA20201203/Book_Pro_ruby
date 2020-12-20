818557a837700c18e3187c8f3c5e2d90d5f57132
MASA20201203 <koedame032@gmail.com>
2020-12-20 11:04:48 +0900

    4.4.2 select/find_all\reject

--- chapter4/chapter4-4.rb
+++ chapter4/chapter4-4.rb
@@ -9,3 +9,14 @@
 # numbers = [1, 2, 3, 4, 6]
 # new_numbers = numbers.map { |n| n * 10}
 # p new_numbers
+
+# 4.4.2 select/find_all\reject
+numbers = [1, 2, 3, 4, 5, 6]
+# ブロックの戻り値が真になった要素だけが集められる
+even_numbers = numbers.select { |n| n.even?}
+p even_numbers
+
+numbers = [1, 2, 3, 4, 5, 6]
+# 3の倍数を除外する（3の倍数以外を集める）
+non_multiples_of_three = numbers.reject { |n| n % 3 == 0 }
+p non_multiples_of_three
