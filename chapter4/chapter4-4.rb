4a342b4458442442acf04dd01f92a33a53055469
MASA20201203 <koedame032@gmail.com>
2020-12-20 10:54:54 +0900

    4.4.1 map/collect

--- chapter4/chapter4-4.rb
+++ chapter4/chapter4-4.rb
@@ -1,2 +1,11 @@
 # 4.4 ブロックを使う配列のメソッド
 
+# 4.4.1 map/collect
+# numbers = [1, 2, 3, 4, 5]
+# new_numbers = []
+# numbers.each { |n| new_numbers << n * 10}
+# p new_numbers
+
+# numbers = [1, 2, 3, 4, 6]
+# new_numbers = numbers.map { |n| n * 10}
+# p new_numbers
