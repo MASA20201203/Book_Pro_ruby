cc0151e1f4dcd6b9bb7c083431ab9c91e33f4aa4
MASA20201203 <koedame032@gmail.com>
2020-12-21 06:52:58 +0900

    4.7.7 メソッドの可変長引数

--- chapter4/chapter4-7.rb
+++ chapter4/chapter4-7.rb
@@ -98,4 +98,10 @@
 # a.push(*b)
 # p a
 
-
+# 4.7.7 メソッドの可変長引数
+# def greeting(*names)
+#   "#{names.join('と')}、こんにちは！"
+# end
+# p greeting('田中さん')
+# p greeting('田中さん', '鈴木さん')
+# p greeting('田中さん', '鈴木さん', '佐藤さん')
