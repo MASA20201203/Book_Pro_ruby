a5850361a27fc5c322771f47161c635e794228ec
MASA20201203 <koedame032@gmail.com>
2020-12-21 16:05:46 +0900

    5.6.2 **でハッシュを展開させる

--- chapter5/chapter5-6.rb
+++ chapter5/chapter5-6.rb
@@ -4,3 +4,9 @@
 # currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
 # p currencies.has_key?(:japan)
 # p currencies.has_key?(:italy)
+
+# 5.6.2 **でハッシュを展開させる
+# h = { us: 'dollar', india: 'rupee' }
+# # 変数hのキーと値を**で展開させる
+# g = { japan: 'yen', **h }
+# p g
