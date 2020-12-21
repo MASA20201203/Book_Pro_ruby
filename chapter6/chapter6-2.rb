b401d415b3470d7b3b1cf1143c3133148e216d72
MASA20201203 <koedame032@gmail.com>
2020-12-22 07:16:32 +0900

    6.2.1 正規表現の便利さを知る

--- chapter6/chapter6-2.rb
+++ chapter6/chapter6-2.rb
@@ -1,1 +1,18 @@
 # 6.2 正規表現って何？
+
+# 6.2.1 正規表現の便利さを知る
+
+# text = <<TEXT
+# I love Ruby.
+# Python is a great language.
+# Java and JavaScript are different.
+# TEXT
+
+# p text.scan(/[A-Z][A-Za-z]+/)
+
+# text = <<TEXT
+# 私の郵便番号は1234567です。
+# 僕の住所は6770056 兵庫県西脇市板波町1234だよ。
+# TEXT
+
+# puts text.gsub(/(\d{3})(\d{4})/, '\1-\2')
