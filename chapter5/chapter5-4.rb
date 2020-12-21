c648a0c55a1984d4ce3d6b1c6be713b659a2a7e7
MASA20201203 <koedame032@gmail.com>
2020-12-21 12:45:31 +0900

    5.4.2 キーや値に異なるデータ型を混在させる

--- chapter5/chapter5-4.rb
+++ chapter5/chapter5-4.rb
@@ -8,3 +8,19 @@
 # # =>ではなく、"シンボル: 値"の記法でハッシュを作成する
 # currencies = { japan: 'yen', us: 'dollar', india: 'rupee'}
 # p currencies[:japan]
+
+# 5.4.2 キーや値に異なるデータ型を混在させる
+# person = {
+#   # 値が文字列
+#   name: 'Alice',
+#   # 値が数値
+#   age: 20,
+#   # 値が配列
+#   friends: ['Bob', 'Carol'],
+#   # 値がハッシュ
+#   phones: { home: '1234-0000', mobile: '5670-0000' }
+# }
+# p person[:age]
+# p person[:friends]
+# p person[:phones][:mobile]
+
