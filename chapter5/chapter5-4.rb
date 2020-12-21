17f1fbbd142db7d85c7352986fbccf6f6782da91
MASA20201203 <koedame032@gmail.com>
2020-12-21 14:08:52 +0900

    5.4.3 メソッドのキーワード引数とハッシュ

--- chapter5/chapter5-4.rb
+++ chapter5/chapter5-4.rb
@@ -24,3 +24,60 @@
 # p person[:friends]
 # p person[:phones][:mobile]
 
+# 5.4.3 メソッドのキーワード引数とハッシュ
+# def buy_burger(menu, drink, potato)
+#   print "#{menu}バーガーを購入"
+#   if drink
+#     print "、ドリンクを購入"
+#   end
+#   if potato
+#     puts "、ポテトを購入"
+#   end
+# end
+
+# # チーズバーガーとドリンクとポテトを購入する
+# buy_burger('cheese', true, true)
+
+# # フィッシュバーガーとドリンクを購入する
+# buy_burger('fish', true, false)
+
+# キーワード引数を指定
+
+# def buy_burger(menu, drink: true, potato: true)
+#   print "#{menu}バーガーを購入"
+#   if drink
+#     print "、ドリンクを購入"
+#   end
+#   if potato
+#     puts "、ポテトを購入"
+#   end
+# end
+
+# # チーズバーガーとドリンクとポテトを購入する
+# buy_burger('cheese', drink: true, potato: true)
+# # フィッシュバーガーとドリンクを購入する
+# buy_burger('fish', drink: true, potato: false)
+
+# デフォルト値を使う
+# buy_burger('cheese')
+# buy_burger('fish', potato: false)
+# buy_burger('fish', potato: false, drink: true)
+
+
+# def buy_burger( menu, drink:, potato: )
+#   print "#{menu}バーガーを購入"
+#   if drink
+#     print "、ドリンクを購入"
+#   end
+#   if potato
+#     puts "、ポテトを購入"
+#   end
+# end
+
+# # デフォルト値を指定しないと、引数が少ないときはエラー
+# # buy_burger('fish', potato: false)
+
+# params = { drink: true, potato: false }
+# buy_burger('fish', params)
+
+
