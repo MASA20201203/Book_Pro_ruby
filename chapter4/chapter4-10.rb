7fa19f56e10845cf1cf40bf6718e478ab7c02466
MASA20201203 <koedame032@gmail.com>
2020-12-21 10:33:00 +0900

    4.10.2 throwとcatchを使った大域脱出

--- chapter4/chapter4-10.rb
+++ chapter4/chapter4-10.rb
@@ -42,3 +42,44 @@
 #     break if n == 3
 #   end
 # end
+
+# 4.10.2 throwとcatchを使った大域脱出
+# fruits = ['apple', 'melon', 'orange']
+# numbers = [1, 2, 3]
+# catch :done do
+#   fruits.shuffle.each do |fruit|
+#     numbers.shuffle.each do |n|
+#       puts "#{fruit}, #{n}"
+#       if fruit == 'orange' && n == 3
+#         # すべての繰り返し処理を脱出する
+#         throw :done
+#       end
+#     end
+#   end
+# end
+
+# fruits = ['apple', 'melon', 'orange']
+# numbers = [1, 2, 3]
+# catch :done do
+#   fruits.shuffle.each do |fruit|
+#     numbers.shuffle.each do |n|
+#       puts "#{fruit}, #{n}"
+#       if fruit == 'orange' && n == 3
+#         # catchと一致しないタグをthrowする
+#         throw :foo
+#       end
+#     end
+#   end
+# end
+
+# ret =
+#   catch :done do
+#     throw :done
+#   end
+# p ret
+
+# ret =
+#   catch :done do
+#     throw :done, 123
+#   end
+# p ret
