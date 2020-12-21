e4d61bcb39a405cc11810e2038fbc08b70d85cdf
MASA20201203 <koedame032@gmail.com>
2020-12-21 12:25:17 +0900

    5.3.1 シンボルと文字列の違い

--- chapter5/chapter5-3.rb
+++ chapter5/chapter5-3.rb
@@ -1,1 +1,23 @@
 # 5.3 シンボル
+
+# 5.3.1 シンボルと文字列の違い
+# p :apple.class
+# p 'apple'.class
+
+# p :apple.object_id
+# p :apple.object_id
+# p :apple.object_id
+
+# p 'apple'.object_id
+# p 'apple'.object_id
+# p 'apple'.object_id
+
+# # 文字列は破壊的な変更が可能
+# string = 'apple'
+# string.upcase!
+# p string
+
+# # シンボルはイミュータブルなので、破壊的な変更は不可能
+# symbol = :apple
+# symbol.upcase!
+# p symbol
