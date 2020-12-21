713f390fdb86249af9d1ef67a7b406419e6cc0be
MASA20201203 <koedame032@gmail.com>
2020-12-21 09:16:17 +0900

    4.8.4 配列がブロック引数に渡される場合

--- chapter4/chapter4-8.rb
+++ chapter4/chapter4-8.rb
@@ -28,3 +28,40 @@
 
 # # mapで処理しつつ、10から始まる添え字を取得する
 # p fruits.map.with_index(10) { |fruit, i| "#{i}: #{fruit}" }
+
+# 4.8.4 配列がブロック引数に渡される場合
+# dimensions = [
+#   # [縦, 横]
+#   [10, 20],
+#   [30, 40],
+#   [50, 60],
+# ]
+
+# # 面積の計算結果を格納する配列
+# areas = []
+# # ブロック引数が1個であれば、ブロック引数の値が配列になる
+# dimensions.each do |dimension|
+#   length = dimension[0]
+#   width = dimension[1]
+#   areas << length * width
+# end
+# p areas
+
+# # 面積の計算結果を格納する配列
+# areas = []
+# # 配列の要素分だけブロック引数を用意すると、各要素の値が別々の変数に格納される
+# dimensions.each do |length, width|
+#   areas << length * width
+# end
+# p areas
+
+# dimensions = [
+#   [10, 20],
+#   [30, 40],
+#   [50, 60],
+# ]
+
+# # ブロック引数を()で囲んで、配列の要素を別々の引数として受け取る
+# dimensions.each_with_index do |(length,width), i|
+#   puts "length: #{length}, width: #{width}, i: #{i}"
+# end
