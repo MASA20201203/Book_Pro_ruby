c6ca395e293b971357a10272f569ecfd60ab6f55
MASA20201203 <koedame032@gmail.com>
2020-12-21 12:38:18 +0900

    5.4.1 ハッシュのキーにシンボルを使う

--- chapter5/chapter5-4.rb
+++ chapter5/chapter5-4.rb
@@ -1,1 +1,10 @@
 # 5.4 続・ハッシュについて
+
+# 5.4.1 ハッシュのキーにシンボルを使う
+# # ハッシュのキーをシンボルにする
+# currencies = { :japan => 'yen', :us => 'dollar', :india => 'rupee'}
+# p currencies[:us]
+
+# # =>ではなく、"シンボル: 値"の記法でハッシュを作成する
+# currencies = { japan: 'yen', us: 'dollar', india: 'rupee'}
+# p currencies[:japan]
