9bbc29705f45ea04872df32105fa1bd1f3fbd835
MASA20201203 <koedame032@gmail.com>
2020-12-23 08:46:32 +0900

    7.4.2 必要なメソッドやクラスを実装する

--- chapter7/gate_test.rb
+++ chapter7/gate_test.rb
@@ -1,12 +1,13 @@
 require 'minitest/autorun'
 require_relative 'gate'
+require_relative 'ticket'
 
 class GateTest < Minitest::Test
   def test_gate
     umeda = Gate.new(:umeda)
     juso = Gate.new(:juso)
     ticket = Ticket.new(150)
-    umeda.ender(ticket)
+    umeda.enter(ticket)
     assert juso.exit(ticket)
   end
 end
