1e401774b24557c4b90a6e212590b0687f272a81
MASA20201203 <koedame032@gmail.com>
2020-12-22 09:04:27 +0900

    初心者歓迎！手と目で覚える正規表現入門・その２「微妙な違いを許容しつつ置換しよう」 - Qiita

--- chapter6/chapter6-2.rb
+++ chapter6/chapter6-2.rb
@@ -31,5 +31,43 @@
 
 # 初心者歓迎！手と目で覚える正規表現入門・その２「微妙な違いを許容しつつ置換しよう」 - Qiita
 
+# text = <<-TEXT
+# クープバゲットのパンは美味しかった。
+# 今日はクープ バゲットさんに行きました。
+# クープ　バゲットのパンは最高。
+# ジャムおじさんのパン、ジャムが入ってた。
+# また行きたいです。クープ・バゲット。
+# クープ・バケットのパン、売り切れだった（><）
+# TEXT
+
+# p text.split(/\n/).grep(/クープ.?バ[ゲケ]ット/)
+
+
+# <select name="game_console">
+# wii_u,Wii U
+# ps4,プレステ4
+# gb,ゲームボーイ
+# </select>]
+
+# <select name="game_console">
+# none,
+# wii_u,Wii U
+# ps4,プレステ4
+# gb,ゲームボーイ
+# </select>
+
+# html = <<-HTML
+# <select name="game_console">
+# <option value="none"></option>
+# <option value="wii_u" selected>Wii U</option>
+# <option value="ps4">プレステ4</option>
+# <option value="gb">ゲームボーイ</option>
+# </select>
+# HTML
+
+# replaced = html.gsub(/<option value="(\w+)"(?: selected)?>(.*)<\/option>/, '\1,\2')
+
+# puts replaced
+
 # 初心者歓迎！手と目で覚える正規表現入門・その３「空白文字を自由自在に操ろう」 - Qiita
 # 初心者歓迎！手と目で覚える正規表現入門・その４（最終回）「中級者テクニックをマスターしよう」 - Qiita
