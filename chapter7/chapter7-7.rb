69276cba4f385c54eaba24b4e96ab42991548b1b
MASA20201203 <koedame032@gmail.com>
2020-12-23 12:40:29 +0900

    7.7.7 protectedメソッド

--- chapter7/chapter7-7.rb
+++ chapter7/chapter7-7.rb
@@ -20,3 +20,33 @@
 #   end
 # end
 # p User.hello
+
+# 7.7.7 protectedメソッド
+# class User
+#   attr_reader :name
+
+#   def initialize(name, weight)
+#     @name = name
+#     @weight = weight
+#   end
+
+#   def heavier_than?(other_user)
+#     other_user.weight < @weight
+#   end
+
+#   protected
+
+#   def weight
+#     @weight
+#   end
+
+# end
+
+# alice = User.new('Alice', 50)
+# bob = User.new('Bob', 60)
+# # p alice.weight
+# # p bob.weight
+# p alice.heavier_than?(bob)
+# p bob.heavier_than?(alice)
+
+# p alice.weight
